1a4f67aa9345d4e6bdb473ec1b0ad150c5651c56
Avinash <avinashrana55@yahoo.com>
2025-06-13 18:41:04 +0530

    Added conditional rendering for isActive fruits

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 const Product = ({ product }) => {
-
-  return (
-    <div className="card" key={product.code}>
-      <img src={product.img} alt={product.name} height={120} width={120} />
-      <span>{product.code}</span>
-      <span>{product.name}</span>
-      <span>{product.price}</span>
-    </div>
-  );
+  return product.isActive && (
+      <div className="card" key={product.code}>
+        <img src={product.img} alt={product.name} height={120} width={120} />
+        <span>{product.code}</span>
+        <span>{product.name}</span>
+        <span>{product.price}</span>
+      </div>
+    );
 };
 
 export default Product;
